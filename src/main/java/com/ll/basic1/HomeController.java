baa531b68ad84c72649b14bcb50056f93af9100f
Hypersand <on8214@gmail.com>
2023-03-14 11:50:57 +0900

    /home/increase, /home/plus

--- src/main/java/com/ll/basic1/HomeController.java
+++ src/main/java/com/ll/basic1/HomeController.java
@@ -3,18 +3,20 @@ package com.ll.basic1;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
 public class HomeController {
 
+    private static int count = 0;
+
     @GetMapping("/home/main")
     @ResponseBody // 리턴값을 응답으로 삼는다.
     public String showMain() {
         return "안녕하세요!";
     }
-
 
     @GetMapping("/home/main2")
     @ResponseBody
@@ -26,6 +28,19 @@ public class HomeController {
     @ResponseBody
     public String showMain3() {
         return "즐거웠습니다.";
+    }
+
+    @GetMapping("/home/increase")
+    @ResponseBody
+    public String showIncrease() {
+        return "응답 : " + count++;
+    }
+
+    @GetMapping("/home/plus")
+    @ResponseBody
+    public String showPlus(@RequestParam int a, @RequestParam int b) {
+        int sum = a + b;
+        return "a + b = " + String.valueOf(sum);
     }
 
 }
