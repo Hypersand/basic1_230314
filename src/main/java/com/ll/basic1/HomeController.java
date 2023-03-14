edc45e2a33ddc49efac51ca649d0a4f24411bea6
Hypersand <on8214@gmail.com>
2023-03-14 13:43:09 +0900

    Person List JSON 반환

--- src/main/java/com/ll/basic1/HomeController.java
+++ src/main/java/com/ll/basic1/HomeController.java
@@ -1,16 +1,22 @@
 package com.ll.basic1;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Controller
 public class HomeController {
 
     private static int count = 0;
+    private static List<Person> list = new ArrayList<>();
 
     @GetMapping("/home/main")
     @ResponseBody // 리턴값을 응답으로 삼는다.
@@ -40,7 +46,34 @@ public class HomeController {
     @ResponseBody
     public String showPlus(@RequestParam int a, @RequestParam int b) {
         int sum = a + b;
-        return "a + b = " + String.valueOf(sum);
+        return "a + b = " + sum;
+    }
+
+
+    @GetMapping("/home/addPerson")
+    @ResponseBody
+    public String addPerson(@RequestParam String name, @RequestParam int age) {
+        Person person = new Person(++count, name, age);
+        list.add(person);
+
+        return count + "번 사람이 추가되었습니다.";
+    }
+
+    @GetMapping("/home/people")
+    @ResponseBody
+    public List<Person> listPerson() {
+        return list;
+    }
+
+
+    @AllArgsConstructor
+    @Data
+    private static class Person {
+        private int id;
+        private String name;
+        private int age;
+
     }
 
 }
+
