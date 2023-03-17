4645aad7e07a6016f07c5697ad559ec63f79c707
Hypersand <on8214@gmail.com>
2023-03-17 14:02:22 +0900

    ArticleService 도입

--- src/main/java/com/ll/basic1/article/controller/ArticleController.java
+++ src/main/java/com/ll/basic1/article/controller/ArticleController.java
@@ -2,11 +2,13 @@ package com.ll.basic1.article.controller;
 
 import com.ll.basic1.article.entity.Article;
 import com.ll.basic1.article.repository.ArticleRepository;
+import com.ll.basic1.article.service.ArticleService;
 import com.ll.basic1.base.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.time.LocalDateTime;
@@ -15,21 +17,16 @@ import java.time.LocalDateTime;
 @RequestMapping("/article")
 @RequiredArgsConstructor
 public class ArticleController {
-    private final ArticleRepository articleRepository;
+    private final ArticleService articleService;
 
     @GetMapping("/write")
     @ResponseBody
-    public RsData write(String title, String body) {
-        Article article = Article
-                .builder()
-                .createDate(LocalDateTime.now())
-                .modifyDate(LocalDateTime.now())
-                .title(title)
-                .body(body)
-                .build();
+    public RsData write(@RequestParam String title, @RequestParam String body) {
 
-        articleRepository.save(article); // INSERT, or UPDATE
+        Article article = articleService.write(title, body);
 
         return RsData.of("S-1", "1번 글이 생성되었습니다.", article);
     }
+
+
 }
